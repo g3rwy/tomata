ab9c824bdc49268e526ab25714703e8b3f7c4541
g3rwy <gerwy@mailbox.org>
2021-06-28 22:30:17 +0200

    Lol it was so stupid to use tuples, its better now

--- src/tomata.nim
+++ src/tomata.nim
@@ -1,39 +1,41 @@
 import nimraylib_now
 const
-  BUFFER_SCALE = 1
+  BUFFER_SCALE = 3
   WIDTH = 600
   HEIGHT = 600
+  STATES_COUNT = 2
 
 type
-  State = tuple
-    idx : uint8
-    color : Color
+  cell_grid = array[ (HEIGHT.toFloat / BUFFER_SCALE.toFloat).toInt , array[ (WIDTH.toFloat / BUFFER_SCALE).toInt , uint8]]
 
-  cell_grid = array[ (HEIGHT.toFloat / BUFFER_SCALE.toFloat).toInt , array[ (WIDTH.toFloat / BUFFER_SCALE).toInt , State]]
 var
   Window_size = Vector2(x:WIDTH,y:HEIGHT)
-  
-  dead : State = (0'u8, Lightgray)
-  alive : State = (1'u8, Black)
-  
   buffer : cell_grid
+  states_colors : array[STATES_COUNT, Color]
 
 setConfigFlags(WINDOW_RESIZABLE)
 initWindow(cint(Window_size.x),cint(Window_size.y), "Game of Life")
 setTargetFPS(60)
 
+states_colors = [
+  Red, # DEAD
+  Green  # ALIVE
+]
+
 proc randomizeGrid(grid : var cell_grid) = 
   for c in grid.mitems:
     for states in c.mitems:
-      states = if getRandomValue(0,1) == 0: dead else: alive
+      states = uint8(getRandomValue(0,1))
 
 proc countN(grid : var cell_grid, x: int, y: int) : int = # Count Neigbhours in donut maner
   var sum = 0;
   for i in -1 .. 1:
     for j in -1 .. 1:
-      sum += cast[int](grid[i + x][j + y].idx)
+      let col = (x + i + buffer.len) mod buffer.len
+      let row = (y + j + buffer[0].len) mod buffer[0].len
+      sum += cast[int](grid[col][row])
   
-  sum -= cast[int](grid[x][y].idx)
+  sum -= cast[int](grid[x][y])
   return sum
 
 buffer.randomizeGrid
@@ -41,7 +43,6 @@ buffer.randomizeGrid
 when isMainModule:
   while not windowShouldClose():
     var next : cell_grid
-    echo "------------------------------------"
     if isWindowResized(): Window_size = (x: getScreenWidth().toFloat, y: getScreenHeight().toFloat)
 
     beginDrawing()
@@ -49,32 +50,29 @@ when isMainModule:
 
     for i ,c in buffer.pairs:
       for j ,states in c.pairs:
-        let x = (BUFFER_SCALE * j)
-        let y = (BUFFER_SCALE * i)
-        drawRectangle(x , y , BUFFER_SCALE , BUFFER_SCALE , states.color)
+        #let x = (BUFFER_SCALE * j)
+        #let y = (BUFFER_SCALE * i)
+        drawRectangle((BUFFER_SCALE * j) ,(BUFFER_SCALE * i) , BUFFER_SCALE , BUFFER_SCALE , states_colors[states])
 
 
 
     for i ,c in buffer.pairs:
       for j ,states in c.pairs:
-        if i == 0 or i == (c.len - 1) or j == 0 or j == (buffer.len - 1):
-          next[i][j] = buffer[i][j]
+        let n = buffer.countN(i,j)
+        #let state = buffer[i][j]
+
+        # RULES 
+        if buffer[i][j] == 0 and n == 3:
+          next[i][j] = 1
+        elif buffer[i][j] == 1 and (n < 2 or n > 3):
+          next[i][j] = 0
         else:
-
-          let n = buffer.countN(i,j)
-          let state = buffer[i][j].idx
-
-          # RULES 
-          if state == 0 and n == 3:
-            next[i][j] = alive
-          elif state == 1 and (n < 2 or n > 3):
-            next[i][j] = dead
-          else:
-            next[i][j] = buffer[i][j]
+          next[i][j] = buffer[i][j]
 
     buffer = next
     if isKeyReleased(Space):
       buffer.randomizeGrid
+    drawFPS(10,10)
     endDrawing()
 
 closeWindow()
