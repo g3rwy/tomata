961085e8596426d2db57d1076865fbde41271e09
g3rwy <gerwy@mailbox.org>
2021-09-01 11:50:20 +0200

    so, chaning for 32-bit floats fixed an issue, now i only need to thread it, maybe i will try doing it with 64-bit floats in future

---
+++ src/burningship.nim
@@ -0,0 +1,188 @@
+import lenientops
+import nimraylib_now
+import math
+import complex
+import simd/x86_avx
+import simd/x86_avx2
+
+from system import compileOption
+
+const
+  WIDTH = 800
+  HEIGHT = 450
+
+
+  range = 3.0
+
+  MaxThreads = 10
+
+var
+  Zoom = 2.0
+  MoveX = 0.0
+  MoveY = 0.0
+
+when WIDTH mod 4 != 0:
+  raise newException(ValueError,"The WIDTH of window must be dividable by 4")
+
+when compileOption("threads"):
+  import locks
+  var thr : array[0 .. MaxThreads - 1, Thread[void]]
+  when WIDTH mod MaxThreads != 0:
+    raise newException(ValueError,"The WIDTH of window must be dividable by amount of Threads")
+
+
+var
+  nIterations : int = 100
+
+  pFractal : array[WIDTH * HEIGHT,int64]
+
+
+initWindow(cint(WIDTH),cint(HEIGHT), "Burning Ship")
+#setTargetFPS(144)
+
+
+proc CreateBurningShip() =
+    let
+        x_scale: float = 1.5 / float64(WIDTH) * Zoom 
+        y_scale: float = 2.0 / float64(HEIGHT) * Zoom 
+
+    for y in 0 ..< HEIGHT:
+        for x in 0 ..< WIDTH:
+            let c = complex(x * x_scale + -2.5 + MoveX, y * y_scale + -1.0 + MoveY)
+            var xtemp : float
+            
+            var
+                z = c
+                n : int = 0 
+            
+            while z.re * z.re + z.im * z.im < 4.0 and n < niterations:
+              xtemp = z.re * z.re - z.im * z.im + c.re
+              z.im = 2.0 * abs(z.re * z.im) + c.im
+              z.re = xtemp
+              inc n
+
+            pFractal[y * WIDTH + x] = n
+
+proc CreateShipSIMD() = 
+  let
+    x_scale: float = 1.5 / float64(WIDTH) * Zoom 
+    y_scale: float = 2.0 / float64(HEIGHT) * Zoom
+
+  var
+    a,b,mask1,two,four: m256
+    zr,zi,zr2,zi2,cr,ci,abs_mask : m256
+
+    c,n,mask2,one,iterations: m256i
+
+    # --- Constants ---
+  one = set1_epi32_256(1)
+
+  two = set1_ps_256(2.0)
+
+  four = set1_ps_256(4.0)
+
+  abs_mask = castsi256_ps(set1_epi32_256(int32.high))
+    # ------------------
+
+  iterations = set1_epi32_256(nIterations)
+
+  for y in 0 ..< HEIGHT:
+    ci = set1_ps_256(y * y_scale + -1.0 + MoveY)
+    for x in countup(0,WIDTH - 1 , 8):
+      # just concept
+      cr = set_ps(x * x_scale + -2.5 + MoveX,(x+1)  * x_scale + -2.5 + MoveX,(x+2)  * x_scale + -2.5 + MoveX, (x+3) * x_scale + -2.5 + MoveX,    (x+4) * x_scale + -2.5 + MoveX,(x+5)  * x_scale + -2.5 + MoveX,(x+6)  * x_scale + -2.5 + MoveX, (x+7) * x_scale + -2.5 + MoveX)
+
+      zr = setzero_ps()
+      zi = setzero_ps()
+      n = setzero_si256()
+
+      while true:
+
+        #[              
+         xtemp = z.re * z.re - z.im * z.im + c.re
+         z.im = 2.0 * abs(z.re * z.im) + c.im
+         z.re = xtemp
+         inc n 
+        ]#
+        zr2 = mul_ps(zr, zr)
+        # (z.re * z.re)
+        
+        zi2 = mul_ps(zi,zi)
+        #(z.im * z.im)
+        
+        a = sub_ps(zr2,zi2) 
+        a = add_ps(a,cr) 
+        
+        b = mul_ps(zr,zi)
+        
+        b = mul_ps(b, two) 
+        #b = castsi256_ps(abs_epi32(castps_si256(b)))
+        b = and_ps(b,abs_mask)
+        b = add_ps(b, ci) 
+        zr = a
+        zi = b
+        
+        a = add_ps(zr2,zi2)
+        
+        mask1 = cmp_ps(a,four,CMP_LT_OQ) 
+
+        mask2 = cmpgt_epi32(iterations,n) 
+        mask2 = and_si256(mask2, castps_si256(mask1)) 
+        
+        c = and_si256(one, mask2)
+        n = add_epi32(n,c)
+        
+        if movemask_ps(castsi256_ps(mask2)) == 0: # if its greater than 0, r
+            break
+
+        let ints = cast[ptr UncheckedArray[int32]](n.addr)
+        pFractal[y * WIDTH + x + 7] = ints[0]
+        pFractal[y * WIDTH + x + 6] = ints[1]
+        pFractal[y * WIDTH + x + 5] = ints[2]
+        pFractal[y * WIDTH + x + 4] = ints[3]
+        pFractal[y * WIDTH + x + 3] = ints[4]
+        pFractal[y * WIDTH + x + 2] = ints[5]
+        pFractal[y * WIDTH + x + 1] = ints[6]
+        pFractal[y * WIDTH + x + 0] = ints[7]
+
+
+while not windowShouldClose():
+  #CreateBurningShip()
+  CreateShipSIMD()
+  if isKeyDown(Space):
+    nIterations += 10
+  elif isKeyDown(Right_Shift):
+    nIterations -= 10
+
+  if isKeyDown(Left):
+    MoveX -= 0.01
+  elif isKeyDown(Right):
+    MoveX += 0.01
+
+  if isKeyDown(Up):
+    MoveY -= 0.01
+  elif isKeyDown(Down):
+    MoveY += 0.01
+
+  if isKeyDown(Right_Bracket):
+    Zoom *= 0.98
+  elif isKeyDown(Left_Bracket):
+    Zoom *= 1.01
+
+  beginDrawing()
+  clearBackground(Raywhite)
+  for x in 0 ..< WIDTH:
+    for y in 0 ..< HEIGHT:
+      let
+        i : int64 = pFractal[y * WIDTH + x]
+        
+      if i == nIterations:
+        drawPixel(x,y,White)
+      else:
+        let i_u8 = uint8(i)
+        drawPixel(x,y,Color(r: i_u8 ,g:  uint8(i_u8 * 1.5),b: uint8(i_u8 * 1.8),a: 255))
+
+  drawText("iterations: " & $nIterations,10,30,10,Violet)  
+  endDrawing()
+
+closeWindow()
