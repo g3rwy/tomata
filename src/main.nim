bd68e49fd0622eb9979e3df658fbe9b793b2e122
g3rwy <gerwy@mailbox.org>
2021-09-06 19:40:03 +0200

    making the main app, barebone script

---
+++ src/main.nim
@@ -0,0 +1,10 @@
+## here is everything for the app
+## Make libraries from other files
+## This should be compiled and run as one GUI app
+
+const
+    W_WIDTH = 1280
+    W_HEIGHT = 720 ## Window size
+    CONTENT_W = 800
+    CONTENT_H = 600 ## Content size (Cellular automata and fractals)
+
