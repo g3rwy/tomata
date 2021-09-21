b6adccbdff6923f4fb08fa42c33bf00ff9e77d0c
g3rwy <gerwy@mailbox.org>
2021-09-21 17:56:47 +0200

    I guess working Game of Life in app, wasn't so hard

--- src/main.nim
+++ src/main.nim
@@ -5,6 +5,7 @@
 
 import nimraylib_now
 import rule as elementar
+import life
 
 const
     W_WIDTH = 1280
@@ -14,9 +15,9 @@ let
     automatas = ["Game of Life", "Ants" , "Elementar CA"]
 
 var
-    curr_tomata = 2
+    curr_tomata = 0
     CONTENT_W = 1000
-    CONTENT_H = 500 ## Content size (Cellular automata and fractals)
+    CONTENT_H = 600 ## Content size (Cellular automata and fractals)
     # ========== Elementar variables =========
     rule : uint8 = 250
 
@@ -38,12 +39,17 @@ while not windowShouldClose():
     drawRectangle(0,W_HEIGHT - CONTENT_H,CONTENT_W,CONTENT_H,Gray)
     
     case curr_tomata:
-    of 0: # Game of Life
-        discard
+    of 0: # Game of Life  
+        for i ,c in base[].pairs:
+            for j ,states in c.pairs:
+        #let x = (BUFFER_SCALE * j)
+        #let y = (BUFFER_SCALE * i)
+                drawRectangle((BUFFER_SCALE * j) ,(BUFFER_SCALE * i) + W_HEIGHT - CONTENT_H, BUFFER_SCALE, BUFFER_SCALE , states_colors[states])
+
+
     of 1: # Ants
         discard
     of 2: # Elementar
-        #drawRectangle(SCALE * 2, SCALE * 2, SCALE,SCALE, cell_color)
         for n in 0 ..< lines_rendered:
             for i in countdown(LastBit, 0): #? thinking of a way to get elementar to scale in good way, with size and all
                 if cached_lines[n].testB(i): drawRectangle(SCALE * i, SCALE * n + W_HEIGHT - CONTENT_H, SCALE,SCALE, cell_color) else: continue
@@ -58,24 +64,38 @@ while not windowShouldClose():
     drawText("Here should be UI",W_WIDTH - 180, 400, 15, Black)
     #=======================================================================================
 
-    if button((x:15.0,y:15.0,width:40.0,height:40.0),"<"):  curr_tomata = (curr_tomata - 1) + (automatas.len * (curr_tomata - 1 < 0).ord)
+    if button((x:15.0,y:15.0,width:40.0,height:40.0),"<"):
+        curr_tomata = (curr_tomata - 1) + (automatas.len * (curr_tomata - 1 < 0).ord)
+        if curr_tomata == 2: # changing content window size for elementar CA
+            CONTENT_W = 1000
+            CONTENT_H = 500
+        else:
+            CONTENT_W = 1000
+            CONTENT_H = 600
     
     let text_padding = (W_WIDTH - measureText(automatas[curr_tomata], 20)) div 2 # Used to center text
     drawText(automatas[curr_tomata], text_padding, 25, 20, Black)
 
-    if button((x:W_WIDTH - 55.0,y:15.0,width:40.0,height:40.0),">"): curr_tomata = (curr_tomata + 1) mod automatas.len # Works
+    if button((x:W_WIDTH - 55.0,y:15.0,width:40.0,height:40.0),">"):
+        curr_tomata = (curr_tomata + 1) mod automatas.len # Works
+        if curr_tomata == 2:
+            CONTENT_W = 1000
+            CONTENT_H = 500
+        else:
+            CONTENT_W = 1000
+            CONTENT_H = 600
     
     endDrawing()
 
     case curr_tomata:
     of 0: # Game of Life
-        discard
+        life.update()
     of 1: # Ants
         discard
     of 2: # Elementar
         if CONTENT_W div CONTENT_H != 2:
             raise newException(ValueError,"!!!!!! the propotions of window must be 1:2 !!!!!!!")
-        update(getFrameTime(),rule)
+        elementar.update(rule)
 
     else:
         discard
