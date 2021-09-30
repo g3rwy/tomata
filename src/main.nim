cad11ed262956b67d0d0b33bf11a490647f25189
g3rwy <gerwy@mailbox.org>
2021-09-30 19:46:43 +0200

    add pause button and moving with arrows

--- src/main.nim
+++ src/main.nim
@@ -28,7 +28,7 @@ setTargetFPS(144)
 
 gen(rule)
 
-proc update_Size(s: int): void = 
+proc update_Size(s: int): void = # Updates the size of Elementar CA cells
   Size = s div 2
   LastBit = Size - 1
   Lines = Size div 2
@@ -56,6 +56,8 @@ while not windowShouldClose():
                 drawRectangle((BUFFER_SCALE * j),(BUFFER_SCALE * i) + W_HEIGHT - CONTENT_H, BUFFER_SCALE , BUFFER_SCALE , cell_colors[place.ord])
 
         drawRectangleLines(cint(BUFFER_SCALE * Ant.y),cint(BUFFER_SCALE * Ant.x + W_HEIGHT - CONTENT_H), BUFFER_SCALE, BUFFER_SCALE, Red)
+
+
     of 2: # Elementar
         for n in 0 ..< lines_rendered:
             for i in countdown(LastBit, 0): #? thinking of a way to get elementar to scale in good way, with size and all
@@ -64,13 +66,15 @@ while not windowShouldClose():
     else:
         discard
 
-    # |------------------ Just for testing ------------------
 
     drawText("Here should be: " & automatas[curr_tomata],(W_HEIGHT - CONTENT_H) div 2 + 100, CONTENT_H div 2, 20, Black)
     drawText("Here should be UI",W_WIDTH - 180, 400, 15, Black)
+    if button((x:W_WIDTH - 255.0,y:150.0,width:70.0,height:40.0),"PAUSE"):
+        life.play = not life.play
+        ants.play = not ants.play
     #=======================================================================================
 
-    if button((x:15.0,y:15.0,width:40.0,height:40.0),"<"):
+    if button((x:15.0,y:15.0,width:40.0,height:40.0),"<") or isKeyReleased(Left):
         curr_tomata = (curr_tomata - 1) + (automatas.len * (curr_tomata - 1 < 0).ord)
         if curr_tomata == 2: # changing content window size for elementar CA
             CONTENT_W = 1000
@@ -82,8 +86,8 @@ while not windowShouldClose():
     let text_padding = (W_WIDTH - measureText(automatas[curr_tomata], 20)) div 2 # Used to center text
     drawText(automatas[curr_tomata], text_padding, 25, 20, Black)
 
-    if button((x:W_WIDTH - 55.0,y:15.0,width:40.0,height:40.0),">"):
-        curr_tomata = (curr_tomata + 1) mod automatas.len # Works
+    if button((x:W_WIDTH - 55.0,y:15.0,width:40.0,height:40.0),">") or isKeyReleased(Right):
+        curr_tomata = (curr_tomata + 1) mod automatas.len # Worksd
         if curr_tomata == 2:
             CONTENT_W = 1000
             CONTENT_H = 500
@@ -95,10 +99,13 @@ while not windowShouldClose():
 
     case curr_tomata:
     of 0: # Game of Life
+
         life.update()
     of 1: # Ants
+
         ants.update()
     of 2: # Elementar
+
         if CONTENT_W div CONTENT_H != 2:
             raise newException(ValueError,"!!!!!! the propotions of window must be 1:2 !!!!!!!")
         elementar.update(rule)
@@ -106,4 +113,4 @@ while not windowShouldClose():
     else:
         discard
 
-closeWindow()## here is everything for the app
+closeWindow()
